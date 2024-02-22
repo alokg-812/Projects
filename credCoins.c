18ff06cf3c61731b40073da1ebccd8b1ccba7148
alokg-812 <154364202+alokg-812@users.noreply.github.com>
2024-02-23 02:12:12 +0530

    cred_coins_modified_code

--- credCoins.c
+++ credCoins.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main() {
-    int n, no_of_coins;
+    int n, total_no_of_coins;
    // printf("Enter n: ");
     scanf("%d",&n);
     for(int i = 0 ; i< n ; i++) {
         //printf("Enter ith: ");
-        scanf("%d",&i);
-      //  printf("Enter the no of bills: ");
-        scanf("%d",&no_of_coins);
-        int k = i*no_of_coins;
-        printf("%d",k/100);
+        int no_of_bills,each_bill;
+        scanf("%d %d",&no_of_bills,&each_bill);
+        total_no_of_coins = no_of_bills * each_bill;
+        printf("%d \n",total_no_of_coins/100);
     }
+    return 0;
 }
